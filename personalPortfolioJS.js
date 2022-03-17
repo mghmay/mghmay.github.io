b53a1b796332fa0a8186d62fa572b3c745549a26
mghmay <mghmay@gmail.com>
2022-03-17 12:14:59 +0000

    Add files via upload

---
+++ personalPortfolioJS.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var prevScrollPos = window.pageYOffset;
+	window.onscroll = function() {
+		var currentScrollPos = window.pageYOffset;
+  		if (prevScrollPos > currentScrollPos) {
+    		document.getElementById("navbar").style.top = "0";
+  		} else {
+    		document.getElementById("navbar").style.top = "-200px";
+  	}
+  	prevScrollPos = currentScrollPos;
+}
+});
